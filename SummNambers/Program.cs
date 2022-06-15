064693c8f6537308ec3af92c84d460295bef4074
Corvette31 <corvette.ss@yandex.ru>
2022-06-15 03:10:26 +0300

    Добавьте файлы проекта.

---
+++ SummNambers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SummNambers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int minValue = 0;
+            int maxValue = 101;
+            int number = rand.Next(minValue, maxValue);
+            int summ = 0;
+
+            Console.WriteLine($"Случайное число : {number}");
+            for (int i = 0; i < number; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    summ += i;
+                    Console.Write($"{i} ");
+                }
+            }
+            Console.WriteLine($"\nСумма всех чисел которые кратные 3 или 5 = {summ}");
+        }
+    }
+}
